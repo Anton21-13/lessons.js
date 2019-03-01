7bab05fa1869ef83f39e5c4baf66be5b31c738a2
Anton21-13 <anton23.pochta@gmail.com>
2019-03-01 23:45:27 +0500

    создал заново уроки по js, начат первый урок

---
+++ lesson_01/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let money = prompt("Ваш бюджет в месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {
+
+  },
+  income = {
+    null,
+  },
+  savings: "false"
+};
+
+
