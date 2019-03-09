8294e749394c2fff31bd076936f2287182a76a1f
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-10 02:12:47 +0500

    Выполнил часть 5 урока

--- lesson_05/half_01/js/script.js
+++ lesson_05/half_01/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let menu = document.querySelector(".menu"),
+  item = document.querySelector(".menu-item"),
+  adv = document.querySelector(".adv"),
+  column = document.querySelectorAll(".column");
+
+
+document.body.style.background = "url(../img/apple_true.jpg) center no-repeat";
+
+let li = document.createElement("li");
+
+li.classList.add("menu-item");
+
+li.innerHTML = "Пятый пункт";
+menu.appendChild(li);
+console.log(li);
+column[1].removeChild(adv);
