06156b4d8dbb5727051ca32999f03883e324a7b7
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-10 14:55:45 +0500

    Доделал вторуючасть урока

--- lesson_05/half_02/js/main.js
+++ lesson_05/half_02/js/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+let btnStart = document.getElementById("start"),
+
+  budget = document.querySelectorAll(".budget-value"),
+  daybudget = document.querySelectorAll(".daybudget-value"),
+  expenses = document.querySelectorAll(".expenses-value"),
+  optionalExpenses = document.querySelectorAll(".optionalexpenses-value"),
+  income = document.querySelectorAll(".income-value"),
+  monthSavings = document.querySelectorAll(".monthsavings-value"),
+  yearSavings = document.querySelectorAll(".yearsavings-value"),
+
+  expenses = document.getElementsByClassName("expenses-item"),
+
+  btn = document.getElementsByTagName("button"),
+  btnExpenses = btn[0],
+  btnOptionalExpenses = btn[1],
+  btnCountBudget = btn[2],
+
+  inputOptionalExpenses = document.querySelectorAll(".optionalexpenses-item"),
+
+  //Получить оставшиеся поля через querySelector (статьи возможного дохода, чекбокс, сумма, процент, год, месяц, день)
+  chooseIncome = document.querySelector("#income"),
+  savings = document.querySelector("#savings"),
+  sum = document.querySelector("#sum"),
+  percent = document.querySelector("#percent"),
+  year = document.querySelector(".year-value"),
+  month = document.querySelector(".month-value"),
+  day = document.querySelector(".day-value");
+
+
+
+
+
+
+ 
