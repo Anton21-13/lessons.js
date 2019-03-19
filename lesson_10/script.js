d9c2635808ca75cd9e596745e884e5cc082f5547
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-19 19:57:34 +0500

    Begining 10 lesson

---
+++ lesson_10/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+let age = document.getElementById('age');
+ 
+function showUser(surname, name) {
+  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+ 
+showUser.apply(age, ['Ritchie' , 'Guy']);
+
+
