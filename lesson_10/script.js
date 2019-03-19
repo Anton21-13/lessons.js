b15b28f6b2c125e38661a1fabec15ec29ea6f8e3
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-19 21:25:34 +0500

    Made 10 lesson

--- lesson_10/script.js
+++ lesson_10/script.js
@@ -1,12 +1,32 @@
-'use strict';
+//'use strict';
 
-
-let age = document.getElementById('age');
- 
-function showUser(surname, name) {
-  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+class Options {
+  constructor(height, width, bg, fontSize, textAlign) {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.textAlign = textAlign;
+  }
+  
+  createDiv() {
+    let newDiv = document.createElement('div');
+    newDiv.style.cssText = `height: ${this.height};
+      width: ${this.width};
+      background-color: ${this.bg};
+      font-size: ${this.fontSize};
+      text-align: ${this.textAlign};`;
+    
+    newDiv.textContent = `Этот квадрат ${this.bg} цвета,
+      его высота ${this.height}, а его ширина ${this.width}.
+      Высота этого текста ${this.fontSize} и он расположен ${this.textAlign}`;
+    document.body.appendChild(newDiv);
+  }
 }
- 
-showUser.apply(age, ['Ritchie' , 'Guy']);
+
+const newOptions = new Options('500px', '1000px', 'green', '25px', 'center');
+
+newOptions.createDiv();
+
 
 
