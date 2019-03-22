c927621d59af3d63ed8f9fd4183488106f7bf501
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-22 23:34:36 +0500

    Made 14th lesson

--- lesson_14/js/script.js
+++ lesson_14/js/script.js
@@ -1,4 +1,19 @@
-
-
+$(document).ready(() => {
   'use strict';
+
+  function openModal() {
+    $('.overlay').fadeIn(1000);
+    $('.modal').slideDown(1000);
+  }
+
+  function closeModal() {
+    $('.overlay').fadeOut(1000);
+    $('.modal').slideUp(1000);
+  }
+
+  $('a[href="#sheldure"], .main_btna, .main_btn').on('click', openModal);
+
+  $('.close').on('click',closeModal);
+
+});
 
